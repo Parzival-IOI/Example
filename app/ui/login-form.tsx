abd9b91600e020c2d4c1c5c388444158d37efa08
Kimhouy Hor <kimhouy.hor@allweb.com.kh>
2024-01-27 23:02:57 +0700

    config login with tailwind

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -14,8 +14,38 @@ import { authenticate } from '@/app/lib/actions';
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
  
-  return (
-    <form action={dispatch} className="space-y-3">
+  return (<>
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {/* <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/> */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Login to Dashboard</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action={dispatch} method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+              <div className="mt-2">
+                <input id="email" name="email" type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                
+              </div>
+              <div className="mt-2">
+                <input id="password" name="password" type="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+              </div>
+            </div>
+
+            <div>
+              <LoginButton />
+            </div>
+          </form>
+        </div>
+      </div>
+    {/* <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -75,7 +105,8 @@ export default function LoginForm() {
           )}
         </div>
       </div>
-    </form>
+    </form> */}
+    </>
   );
 }
  
@@ -83,8 +114,10 @@ function LoginButton() {
   const { pending } = useFormStatus();
  
   return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+    <button type="submit" aria-disabled={pending} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+
+    // <Button className="mt-4 w-full" aria-disabled={pending}>
+    //   Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    // </Button>
   );
 }
