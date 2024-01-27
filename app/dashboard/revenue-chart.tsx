d7fea4758405afef6eab56367206d4bc8b27bf22
Kimhouy Hor <kimhouy.hor@allweb.com.kh>
2024-01-27 22:34:43 +0700

    full tutorial is completed

--- app/dashboard/revenue-chart.tsx
+++
@@ -1,8 +0,0 @@
-
-const revenuechart = () => {
-  return (
-    <div>revenue-chart</div>
-  )
-}
-
-export default revenuechart
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Parzival Dashboard',
+  description: 'Personal Dashboard for Parzival',
+  metadataBase: new URL('https://parzival-dash.vercel.app/'),
+};
+
 export default function RootLayout({
   children,
 }: {
